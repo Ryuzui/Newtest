d4e97e932be9c0173be69d588fdaf3e3ffd6ca18
stu <stu@qq.com>
2016-10-19 11:42:13 +0800

    增加新闻列表

--- Newtest/Controllers/HomeController.cs
+++ Newtest/Controllers/HomeController.cs
@@ -8,29 +8,57 @@ namespace Newtest.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// 主页
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View();
         }
-
+        /// <summary>
+        /// 关于
+        /// </summary>
+        /// <returns></returns>
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
+        /// <summary>
+        /// 联系方式
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
-
+        /// <summary>
+        /// Welcome
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Welcome()
         {
 
             return View();
         }
+        /// <summary>
+        /// 新闻列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult List()
+        {
+            String[] data = new string[] { "在Linux开发.NET——拜拜了Win10",
+                "微软开源LightGBM：轻量级梯度Boosting框架",
+                "Go 1.7.2 版本发布了。",
+                "Git 项目推荐 | lyadmin 轻量级通用后台" };
+            ViewBag.data = data;
+
+            return View();
+        }
+
     }
 }
