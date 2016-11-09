62c7182c6111649b7b19dffe6cc565ea2963eb7e
1351300108刘瑞 <shenfanlr@hotmail.com>
2016-11-09 12:26:20 +0800

    使用Cookie做身份验证

---
+++ Newtest/cookieDatabase.cs
@@ -0,0 +1,20 @@
+﻿namespace Newtest
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class CookieDatabase : DbContext
+    {
+
+     
+          
+
+        public CookieDatabase()
+            : base("name=cookieDatabase")
+        {
+        }
+
+    }
+
+}
